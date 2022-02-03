07f7dd998ce6dd54c92bd596d5ff3a227568ff6a
ArtemDzeryzhinski <98953236+ArtemDzeryzhinski@users.noreply.github.com>
2022-02-03 19:00:20 +0300

    Add files via upload

---
+++ Homework1_v1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Spawner : MonoBehaviour
+{
+    public Text text;
+
+    public Transform sp;
+    private int number;
+    public GameObject[] Prefabs;
+    private GameObject Inst;
+   
+    public void Spawn()
+    {
+ 
+            if(Inst != null)
+            {
+                Destroy(Inst);
+
+            }
+ 
+            number = Random.Range(0, Prefabs.Length);
+            Inst = Instantiate(Prefabs[number], sp.position, Quaternion.identity);
+           
+            text.text = Prefabs[number].name;
+        
+    }
+}
